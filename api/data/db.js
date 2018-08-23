37e30e1c471ed918be38d98c3a15ec9e917ff5d9
denisaroman <denisa-maria.roman.14@ucl.ac.uk>
2018-08-23 11:22:05 +0300

    1

--- api/data/db.js
+++ api/data/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var dburl = 'mongodb://localhost:27017/Formulary';
+var dburl = 'mongodb://denisaroman:Summerpj1@ds125352.mlab.com:25352/formulary';
 var retry = null;
 mongoose.connect(dburl);
 
